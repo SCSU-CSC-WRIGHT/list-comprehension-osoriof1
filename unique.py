01ad763832a89a8cb6a3fa84e30bf0f6094fb093
osoriof1 <osoriof1@southernct.edu>
2024-11-20 21:36:46 +0000

    done

--- unique.py
+++ unique.py
@@ -6,11 +6,15 @@ def lab_3():
     for num in numero_list:
         if num in numbers_list: 
             numbers_list.remove(num)
-
+    
     print (numero_list + numbers_list)
+    user = input("Continue?: ")
+    if user == ("yes"):
+        lab_3()
+    elif user == ("no"):
+        print("Bye")
+        
 
 lab_3()
 
-# 1. Create input
-# 2. Create list and use split
-# 3. Use loop to check for duplicate number (blank.remove(num))
+
